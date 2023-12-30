de6f2b77527144635d13e6286255ea6cfa4ed77b
huuzey <weleladinsefa@gmail.com>
2023-12-30 12:14:08 +0300

    document

--- src/app/components/Top.jsx
+++ src/app/components/Top.jsx
@@ -8,7 +8,6 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import dynamic from "next/dynamic";
 
 import CanvasJS from "@canvasjs/charts";
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
-import Top from "./components/Top";
-import Sectionmd from "./components/Sectionmid.js";
-import Third from "./components/Third";
+import dynamic from "next/dynamic";
 
+const Top = dynamic(() => import("./components/Top"), { ssr: false });
+const Sectionmd = dynamic(() => import("./components/Sectionmid.js"), {
+  ssr: false,
+});
+const Third = dynamic(() => import("./components/Third"), { ssr: false });
 export default async function Home() {
   return (
     <div className="mt-4 mx-6 ">
